570327940c2623c1b352a5583ebb634a41af8fa4
amora92 <amocodes@yahoo.com>
2023-03-23 16:27:11 +0000

    added sentences,fonts, css

--- src/components/landing-page/LandingPage.js
+++ src/components/landing-page/LandingPage.js
@@ -7,6 +7,8 @@ const LandingPage = () => {
   return (
     <div className="landing-page">
       <div className="image-section"></div>
+      <h1 className="wedding-planner">Wedding Planner</h1>
+      <h2 className="plan-your-own-wedding">Plan Your Own Wedding</h2>
       <button className="enter-button" onClick={() => console.log('Enter button clicked')}>
         Enter
       </button>
