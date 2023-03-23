3bf85366467c9904f3b4b9d690e41d7d102cb70b
amora92 <amocodes@yahoo.com>
2023-03-23 16:49:06 +0000

    fixing-image-positioning

--- src/components/landing-page/LandingPage.js
+++ src/components/landing-page/LandingPage.js
@@ -6,7 +6,9 @@ import './LandingPage.css';
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <div className="image-section"></div>
+      <div className="image-container">
+        <div className="image-section"></div>
+      </div>
       <h1 className="wedding-planner">Wedding Planner</h1>
       <h2 className="plan-your-own-wedding">Plan Your Own Wedding</h2>
       <button className="enter-button" onClick={() => console.log('Enter button clicked')}>
