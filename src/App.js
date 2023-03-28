30cb1d3dda9cbb3a8d11e6e42b49b0d56123c3e8
amora92 <amocodes@yahoo.com>
2023-03-28 14:50:27 +0100

    Form component
    js css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './/components/landing-page/LandingPage';
+import SignUp from './/components/sign-up/SignUp';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <SignUp />
     </div>
   );
 }
