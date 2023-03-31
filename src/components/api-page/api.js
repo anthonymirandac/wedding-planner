24a6ae70a20ff01bf93054278a3bab58c5a0be29
Khatra Ahmed <[khatra.ahmed@gmail.com]>
2023-03-31 12:46:27 +0100

    Working on api

--- src/components/api-page/api.js
+++ src/components/api-page/api.js
@@ -27,41 +27,4 @@
 // 	console.log(body);
 //     console.log(response);
 // });
-const axios = require("axios");
-
-const options = {
-  method: 'GET',
-  url: 'https://local-business-data.p.rapidapi.com/search',
-  params: {
-    query: 'Wedding Venues in London, UK',
-    limit: '20',
-    lat: '37.359428',
-    lng: '-121.925337',
-    zoom: '13',
-    language: 'en',
-    region: 'uk'
-  },
-  headers: {
-    'X-RapidAPI-Key': 'da9f223eecmsh2df64a8471b61d2p1d305djsnc532ac13ff0f',
-    'X-RapidAPI-Host': 'local-business-data.p.rapidapi.com'
-  }
-};
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-
-
-
-// export default api;
-
-// varaiables to access
-const form = document.querySelector('form');
-
-// Event Listener for user input
-
-//Fetch required data 
-
-// Show the data provided by the API to the user
 
--- src/components/couples-questionnaire/CouplesQuestionnaire.js
+++ src/components/couples-questionnaire/CouplesQuestionnaire.js
@@ -4,12 +4,52 @@ import React from 'react';
 import './CouplesQuestionnaire.css';
 import Female from './Female.png';
 import Male from './Male.png';
+import {useState} from 'react';
+import {useEffect} from 'react';
+
 
 const CouplesQuestionnaire = () => {
+  const [endPoint,setEndPoint] = useState('');
+  const [container, setContainer] = useState([]);
+  const [finalPoint,setFinalPoint] = useState('');
+
+
+  useEffect(() => {
+    fetchMe()
+  },[finalPoint])
+
+
+  const fetchMe = () => {
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '0b7fd3dc1emshdd7a36216956795p1969d1jsn1b11d19810f2',
+      'X-RapidAPI-Host': 'local-business-data.p.rapidapi.com'
+    }
+  };
+  
+  fetch(`https://local-business-data.p.rapidapi.com/search?query=Wedding%20Venues%20in%20${endPoint}limit=6&lat=37.359428&lng=-121.925337&zoom=13&language=en&region=uk`, options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .then(data => {
+      setContainer(data)
+    })
+    .catch(err => console.error(err));
+  }
+    const onChangeHandler = (e) => {
+      setEndPoint(e.target.value)
+    }
+
+    const submitHandler = e => {
+      e.preventDefault()
+      setFinalPoint(endPoint)
+    }
+
   return (
     <div className="questionnaire">
       <h1 className="questionnaire-title">Couples Questionnaire</h1>
-      <form className="questionnaire-form">
+      <form onSubmit={submitHandler} className="questionnaire-form">
         <div className="gender-select">
         <label>
             <input type="radio" name="gender" value="male" />
@@ -21,10 +61,18 @@ const CouplesQuestionnaire = () => {
           </label>        </div>
         <input type="text" className="questionnaire-input" placeholder="Couple's Name" />
         <input type="text" className="questionnaire-input" placeholder="Wedding Theme" />
-        <input type="text" className="questionnaire-input" placeholder="Wedding Location" />
+        <input type="text" value={endPoint} onChange={onChangeHandler} className="questionnaire-input"/>
         <input type="date" className="questionnaire-input" placeholder="Wedding Date" />
         <button className="submit-button" type="submit">Submit</button>
       </form>
+      {container?.map((item) => {
+        return (
+          <div>
+            <img src={item.photos_sample.photo_url} alt="" />
+            <p>{item.name}</p>
+          </div>
+        )
+      })}
     </div>
   );
 };
