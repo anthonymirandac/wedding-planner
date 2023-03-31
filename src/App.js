2eb57637c7f8b7fbbd9f19c9efdada95d3936d21
Khatra Ahmed <[khatra.ahmed@gmail.com]>
2023-03-31 09:36:50 +0100

    Fetching API

--- src/App.js
+++ src/App.js
@@ -9,6 +9,20 @@ import ConfirmationBooking from './/components/confirmation-booking/Confirmation
 
 
 function App() {
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'f60ea775aemshfa16373cb9354f5p10df1djsn7f5037b1dce2',
+      'X-RapidAPI-Host': 'local-business-data.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://local-business-data.p.rapidapi.com/search?query=Wedding%20Venues%20in%20London%2C%20UK&limit=20&lat=37.359428&lng=-121.925337&zoom=13&language=en&region=uk', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
   return (
     <div className="App">
       <LandingPage />
